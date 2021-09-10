ff5b7886c21b11a42778cdeeb0bf401bb83703f2
BVRXNDVRK <70912089+BVRXNDVRK@users.noreply.github.com>
2021-09-10 13:31:34 +0300

    third lesson

--- script/script.js
+++ script/script.js
@@ -14,6 +14,32 @@
   const secondBurgerName = `Черный`;
   const nextButton = document.querySelector('#next');
   const prevButton = document.querySelector('#prev');
+  const sendButton = document.querySelector('#send');
+
+  let myVar = 3;
+
+  // if(myVar < 3) {
+  //   console.log('myVar < 3');
+  // }
+
+  // if (myVar === 3) {
+  //   console.log('myVar = 3');
+  // }
+
+  // if (myVar > 3) {
+  //   console.log('myVar > 3');
+  // }
+
+  switch(true) {
+    case (myVar === 1):
+      console.log(' < 3');
+      break;
+    case (myVar === 3):
+      console.log(' = 3');
+      break;
+    default: 
+      console.log('None');
+  }
 
   // OBJECTS&MASSIVES
   const questions = [
@@ -101,17 +127,17 @@
   })
 
   const playTest = function () {
-
+    const finalAnswers = [];
     let numberQuestion = 0;
 
     const renderAnswers = (index) => {
       questions[index].answers.forEach((answer) => {
         const answerItem = document.createElement('div');
-        answerItem.classList.add('answers-item', 'd-flex', 'flex-column');
+        answerItem.classList.add('answers-item', 'd-flex', 'justify-content-center');
 
         answerItem.innerHTML = `
-                <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none">
-                <label for="${answer.title} class="d-flex flex-column justify-content-between">
+                <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none" value="${answer.title}">
+                <label for="${answer.title}" class="d-flex flex-column justify-content-between">
                   <img class="answerImg" src="${answer.url}" alt="burger">
                   <span>${answer.title}</span>
                 </label>
@@ -124,16 +150,38 @@
     const renderQuestions = (indexQuestion) => {
       formAnswers.innerHTML = '';
 
-      questionTitle.textContent = questions[indexQuestion].question;
-
-      renderAnswers(indexQuestion);
+      if (numberQuestion >= 0 && numberQuestion <= questions.length - 1) {
+        questionTitle.textContent = questions[indexQuestion].question;
+        renderAnswers(indexQuestion);
+      }
     }
     renderQuestions(numberQuestion);
 
-    // HOMEWORK <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    const checkAnswer = () => {
+      const obj = {};
+
+      const inputs = [...formAnswers.elements].filter((input) => input.checked || input.id === 'numberPhone');
+
+      inputs.forEach((input, index) => {
+        if (numberQuestion >= 0 && numberQuestion <= questions.length - 1) {
+          obj[`${index}_${questions[numberQuestion].question}`] = input.value;
+        }
+
+        if (numberQuestion === questions.length) {
+          obj['[Номер телефона:'] = input.value;
+        }
+      })
+
+      finalAnswers.push(obj)
+      console.log(finalAnswers);
+    }
+    
     prevButton.classList.add('d-none');
+    sendButton.classList.add('d-none');
+    nextButton.classList.remove('d-none');
     
     nextButton.onclick = () => {
+      checkAnswer();
       numberQuestion++;
       renderQuestions(numberQuestion);
 
@@ -141,21 +189,44 @@
       prevButton.classList.remove('d-none');
       }
 
-      if (numberQuestion == (questions.length - 1) ) {
+      if (numberQuestion === (questions.length) ) {
       nextButton.classList.add('d-none');
+      questionTitle.textContent = 'СПАСИБО!';
+      sendButton.classList.remove('d-none');
+      formAnswers.innerHTML = `
+      <div>
+        <label for="numberPhone">Пожалуйста, ведите ваш номер телефона :)</label>
+        <input type="phone" class="form-control" id="numberPhone">
+      </div>
+      `
       }
     }
 
     prevButton.onclick = () => {
       numberQuestion--;
-      renderQuestions(numberQuestion)
-      if (numberQuestion < (questions.length - 1) ) {
+      renderQuestions(numberQuestion);
+      if (numberQuestion < (questions.length) ) {
       nextButton.classList.remove('d-none');
+      sendButton.classList.add('d-none');
       }
       if (numberQuestion == 0) {
       prevButton.classList.add('d-none');
     }
     }
+
+    sendButton.addEventListener('click', () => {
+      checkAnswer();
+      numberQuestion++;
+      renderQuestions(numberQuestion);
+      console.log(finalAnswers);
+
+      if(numberQuestion === questions.length + 1) {
+        formAnswers.textContent = 'Ожидайте, скоро с вами свяжется наш менеджер!';
+        setTimeout(() => {
+          modalBlock.classList.remove('d-block');
+        }, 2000);
+      }
+    })
     
   }
 })
